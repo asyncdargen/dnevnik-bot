bfff15561f98b745dad11140ea264fa71684e4b3
Andrew Vasilev <dargencode@mail.ru>
2022-09-27 16:45:24 +0300

    remove scripts

--- run.bat
+++
@@ -1,8 +0,0 @@
-set TELEGRAM_TOKEN=your telegram bot token
-
-set MOS_LOGIN=your mos.ru login
-set MOS_PASSWORD=your mos.ru password
-
-pip install --ignore-installed -r requires.txt
-
-python main.py
